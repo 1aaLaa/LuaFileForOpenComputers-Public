b052da88d90b2a11025addf272cf0e78817605eb
1aaLaa <95925328+1aaLaa@users.noreply.github.com>
2025-10-04 17:07:31 +0100

    Create recipe.lua

---
+++ OC-Autocrafting/recipe.lua
@@ -0,0 +1,7 @@
+craftWithPattern(
+    5,  -- pattern is in slot 5 of buffer chest
+    { 
+      {"minecraft:iron_ingot", 3, 1},  -- 3 iron ingots from slot 1
+      {"minecraft:stick", 2, 2}       -- 2 sticks from slot 2
+    }
+)
